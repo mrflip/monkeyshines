5536ecdef739b240363bf7fd01fe2ead9a99821c
Philip (flip) Kromer <flip@infochimps.org>
2009-07-08 16:37:57 -0500

    OK basic structure is in place

--- lib/monkeyshines/scrape.rb
+++
@@ -1,8 +0,0 @@
-require 'twitter_friends/scrape/twitter_api'
-require 'twitter_friends/scrape/expanded_url'
-require 'twitter_friends/scrape/http_scraper'
-require 'twitter_friends/scrape/scrape_dumper'
-require 'twitter_friends/scrape/scrape_request'
-
-require 'twitter_friends/scrape/scraped_file'
-require 'twitter_friends/scrape/scrape_store'
---
+++ lib/monkeyshines/scrape_engine/http_head_scraper.rb
@@ -0,0 +1,25 @@
+module Monkeyshines
+  module Scraper
+    class HttpHeadScraper
+
+      #
+      # Get the redirect location... don't follow it, just request and store it.
+      #
+      def fetch_dest_url! options={ }
+        return unless dest_url.blank? && scraped_at.blank?
+        options = options.reverse_merge :sleep => 1
+        fix_src_url!
+        begin
+          # look for the redirect
+          raw_dest_url = Net::HTTP.get_response(URI.parse(src_url))["location"]
+          self.dest_url = self.class.scrub_url(raw_dest_url)
+          sleep options[:sleep]
+        rescue Exception => e
+          nil
+        end
+        self.scraped_at = TwitterFriends::StructModel::ModelCommon.flatten_date(DateTime.now) if self.scraped_at.blank?
+      end
+      
+    end
+  end
+end
