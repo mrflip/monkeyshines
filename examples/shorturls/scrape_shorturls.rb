f86ae317610f021cb61b32f9b0421784b192dc86
Philip (flip) Kromer <flip@infochimps.org>
2009-07-12 23:40:41 -0500

    Logging

--- examples/shorturls/scrape_shorturls.rb
+++ examples/shorturls/scrape_shorturls.rb
@@ -9,38 +9,43 @@ require 'shorturl_request'
 require 'shorturl_sequence'
 require 'trollop' # gem install trollop
 
+#
+# Example usage:
+#
+#    nohup ./scrape_shorturls.rb --base-url='http://tinyurl.com/' --encoding_radix=36 \
+#      --create-db=true --store-db rawd/shorturl_scrapes-sequential-`datename`.tdb
+#      --max-limit=1200000000 --min-limit=200000000  >> log/shorturl_scrapes-sequential-`datename`.log &
+#
+#
 opts = Trollop::options do
-  opt :from,      "Flat file of scrapes",                      :type => String
-  opt :store_db,  "Tokyo cabinet db name",                     :type => String
-  opt :create_db, "Create Tokyo cabinet if --store-db doesn\'t exist?", :type => String
-  opt :skip,      "Initial requests to skip ahead",            :type => Integer
-  opt :base_url,  "First part of URL incl. scheme and trailing slash, eg http://tinyurl.com/", :type => String
-  opt :min_limit, "Smallest sequential URL to randomly visit", :type => Integer
-  opt :max_limit, "Largest sequential URL to randomly visit",  :type => Integer
-  opt :encoding_radix, "Modulo for turning int index into tinyurl string",  :type => Integer
+  opt :from,            "Flat file of scrapes",                                                      :type => String
+  opt :store_db,        "Tokyo cabinet db name",                                                     :type => String
+  opt :create_db,       "Create Tokyo cabinet if --store-db doesn\'t exist?",                        :type => String
+  opt :skip,            "Initial requests to skip ahead",                                            :type => Integer
+  opt :base_url,        "First part of URL incl. scheme and trailing slash, eg http://tinyurl.com/", :type => String
+  opt :min_limit,       "Smallest sequential URL to randomly visit",                                 :type => Integer
+  opt :max_limit,       "Largest sequential URL to randomly visit",                                  :type => Integer
+  opt :encoding_radix,  "Modulo for turning int index into tinyurl string",                          :type => Integer
 end
 
 # Request stream
 #request_stream = Monkeyshines::FlatFileRequestStream.new_from_command_line(opts, :request_klass => ShorturlRequest)
-# request_stream = SequentialUrlRequestStream.new('http://tinyurl.com/', ('aaaaaa'..'lszzzz'))
-
-# nohup ./scrape_shorturls.rb --base-url='http://tinyurl.com/' --max-limit=1200000000 --min-limit=200000000 --create-db=true --store-db rawd/shorturl_scrapes-sequential-`datename`.tdb >> log/shorturl_scrapes-sequential-`datename`.log &
 request_stream = RandomSequentialUrlRequestStream.new_from_command_line opts, :request_klass => ShorturlRequest
 
 # Scrape Store
-store = Monkeyshines::ScrapeStore::ReadThruStore.new_from_command_line opts
+store   = Monkeyshines::ScrapeStore::ReadThruStore.new_from_command_line opts
 
 # Scraper
 scraper = Monkeyshines::ScrapeEngine::HttpHeadScraper.new
 
 # Bulk load into read-thru cache.
 Monkeyshines.logger.info "Beginning scrape itself"
-Monkeyshines.log_every 10, :scrape_request, :starting_at => opts[:skip]
+Monkeyshines.log_occasional_begin :scrape_requests, :every => 10, :starting_at => opts[:skip]
 request_stream.each do |scrape_request|
   # next if scrape_request.url =~ %r{\Ahttp://(poprl.com|short.to|timesurl.at)}
   result = store.set( scrape_request.url ){ scraper.get(scrape_request) }
   Monkeyshines.log_occasional(:scrape_request){|iter| [iter, store.size, scrape_request.response_code, result, scrape_request.url].join("\t") }
-  # sleep 0.1
+  sleep 0.1
 end
 store.close
 scraper.finish
--- lib/monkeyshines.rb
+++ lib/monkeyshines.rb
@@ -12,17 +12,54 @@ module Monkeyshines
     @logger = logger
   end
 
-  LOG_INTERVAL = { }
-  LOG_COUNTER  = { }
+  OCCASIONAL_LOG_INTERVAL = { }
+  OCCASIONAL_LOG_COUNTER  = { }
   #
-  def self.log_every interval, tracking_token, options={}
-    LOG_INTERVAL[tracking_token] = interval
-    LOG_COUNTER[ tracking_token] = options[:starting_at] || 0
+  # Sets up interval and starting value for log_occasional
+  #
+  # options[:every] sets how often the log is written
+  # options[:starting_at] sets an initial value for the iteration
+  #
+  #
+  def self.log_occasional_begin tracking_token, options={}
+    OCCASIONAL_LOG_INTERVAL[tracking_token] = options[:every]       || 100
+    OCCASIONAL_LOG_COUNTER[ tracking_token] = options[:starting_at] || 0
   end
 
+  #
+  # Using values set in log_occasional_begin.
+  # Logs every N'th time this is called with the corresponding token.
+  #
+  # If N calls have passed, calls &block with the (pos-increment) iterator as
+  # argument.
+  #
+  # Ex.
+  #     log_occasional_begin :requests, :every => 100, :starting_at => resume_point||0
+  #     loop do
+  #       # ... stuff ...
+  #       log_occasional(:requests){|iter| [iter, request.url, result.length].join("\t") }
+  #     end
+  #
   def self.log_occasional tracking_token, &block
-    if ((LOG_COUNTER[tracking_token] += 1) % LOG_INTERVAL[tracking_token] == 0)
-      logger.info block.call(LOG_COUNTER[tracking_token])
+    if ((OCCASIONAL_LOG_COUNTER[tracking_token] += 1) % OCCASIONAL_LOG_INTERVAL[tracking_token] == 0)
+      logger.info block.call(OCCASIONAL_LOG_COUNTER[tracking_token])
+    end
+  end
+
+  PERIODIC_LOG_TIMES = {}
+  #
+  # Ex: log if it has been at least 5 minutes since last announcement:
+  #   loop do
+  #     # ... stuff ...
+  #     Monkeyshines.log_periodically(:stuff, 300){ [radiosity, luminance, bifurcation].join("\t") }
+  #   end
+  #
+  def self.log_periodically tracking_token, interval_in_seconds, &block
+    curr  = Time.now.utc.to_f
+    prev   = (PERIODIC_LOG_TIMES[tracking_token]||=curr)
+    if (since = curr-prev) > interval_in_seconds
+      logger.info block.call(since)
+      PERIODIC_LOG_TIMES[tracking_token] = curr
     end
   end
 end
