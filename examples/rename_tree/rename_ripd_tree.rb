b1b9092bb9060774ee571c113c880446ddb8d9fe
Philip (flip) Kromer <flip@infochimps.org>
2009-07-26 18:25:00 -0500

    Specializing. hrm.

--- examples/rename_tree/rename_ripd_tree.rb
+++ examples/rename_tree/rename_ripd_tree.rb
@@ -28,11 +28,12 @@ RIPD_ROOT = '/data/ripd'
 #
 old_filename_pats = {
   RIPD_ROOT+'/_com/_tw/bundled/_200*/**/*' =>
-    RIPD_ROOT+'/_com/_tw/:handle/bundled/_:date/_:hour/bundle+:timestamp.scrape.:ext',
+  { :patt => RIPD_ROOT+'/_com/_tw/:handle/bundled/_:date/_:hour/bundle+:timestamp.scrape.:ext'},
   RIPD_ROOT+'/com.tw/com.twitter.stream/hosebird-*' =>
-    RIPD_ROOT+'/com.tw/:handle/hosebird-:date-:time.:ext',
+  { :patt => RIPD_ROOT+'/com.tw/:handle/hosebird-:date-:time.:ext' },
   RIPD_ROOT+'/com.tw/com.twitter.search/*/com.twitter.search+*[^r].tsv' =>
-    RIPD_ROOT+'/com.tw/:handle/:date/:handle+:timestamp-:pid.:ext',
+  { :patt => RIPD_ROOT+'/com.tw/:handle/:date/:handle+:timestamp-:pid.:ext',
+    :toks => { :hostname => 'womper' } }
 }
 
 #
@@ -61,19 +62,18 @@ def fix_filename_tokens! filename_tokens
   if (!filename_tokens[:timestamp]) && (filename_tokens[:date] || filename_tokens[:time])
     filename_tokens[:timestamp] = "%s%s" % [filename_tokens[:date], filename_tokens[:time]]
   end
-  filename_tokens.merge! :hostname => 'womper'
 end
 
 #
 # Do this thing
 #
-old_filename_pats.each do |files_to_rename, pat_str|
-  old_filename_pat = FilenamePattern.new(pat_str)
+old_filename_pats.each do |files_to_rename, instructions|
+  old_filename_pat = FilenamePattern.new(instructions[:patt])
   Monkeyshines.logger.info "Renaming files matching #{files_to_rename}"
   Dir[files_to_rename].sort.each do |old_filename|
     next unless File.file?(old_filename)
     filename_tokens = old_filename_pat.recognize(old_filename) or next
-    fix_filename_tokens! filename_tokens
+    fix_filename_tokens! filename_tokens.merge(instructions[:toks])
     new_filename = new_filename_pat.make(filename_tokens)
     rename_carefully old_filename, new_filename, opts[:dry_run]
   end
