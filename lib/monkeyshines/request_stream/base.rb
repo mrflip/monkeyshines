a7389deb460af10a19258a010a4511741d47f056
Philip (flip) Kromer <flip@infochimps.org>
2009-07-31 11:30:26 -0500

    Abstracting scrape runner

--- lib/monkeyshines/request_stream/base.rb
+++ lib/monkeyshines/request_stream/base.rb
@@ -3,8 +3,10 @@ module Monkeyshines
 
     class Base
       attr_accessor :request_klass
-      def initialize request_klass, options={}
+      attr_accessor :request_store
+      def initialize request_klass, request_store, options={}
         self.request_klass = request_klass
+        self.request_store = request_store
       end
 
       def self.new_from_command_line cmdline_opts, default_opts={}
@@ -13,6 +15,12 @@ module Monkeyshines
         request_stream = Monkeyshines::FlatFileRequestStream.new(options[:from], options[:request_klass])
         request_stream.skip! options[:skip] if options[:skip].to_i > 0
         request_stream
+      end
+
+      def each *args, &block
+        self.request_store.each(*args) do |req_params|
+          yield request_klass.new(req_params)
+        end
       end
     end
 
---
+++ lib/monkeyshines/runner/base.rb
@@ -0,0 +1,114 @@
+require 'trollop'
+require 'yaml'
+require 'wukong'
+require 'monkeyshines/utils/uri'
+require 'monkeyshines/utils/filename_pattern'
+require 'monkeyshines/scrape_store/conditional_store'
+require 'monkeyshines/scrape_engine/http_head_scraper'
+module Monkeyshines
+  module Runner
+
+    DEFAULT_OPTS = {
+
+      :src_type          => :flat_file_store,
+      :src_name          => nil,
+      :src_skip          => nil,
+
+      #
+      :scraper           => :http_scraper,
+
+      #
+      :dest_type         => :conditional_store
+      :dest_cache_name   => nil,
+      :dest_filename     => nil,
+      :dest_pattern      => nil,
+      :dest_pattern_root => nil,
+      :dest_chunk_time   => nil,
+    }
+
+    #
+    # A scraper
+    #
+    # * loads ScrapeRequests from a request_src
+    # *
+    #
+    class Base
+      attr_accessor :opts
+      def initialize opts
+        self.opts = opts
+      end
+
+      #
+      #
+      #
+      def standard_opts
+        opts = Trollop::options do
+          opt :log,            "Log file name; leave blank to use STDERR",     :type => String
+          # input from file
+          opt :src_name,       "URI for scrape store to load from",            :type => String
+          opt :src_skip,       "Initial lines to skip",                        :type => Integer
+          # output storage
+          opt :cache_loc,      "URI for cache server",                         :type => String, :default => ':10022'
+          opt :chunk_time,     "Frequency to rotate chunk files (in seconds)", :type => Integer, :default => 60*60*4
+          opt :dest_dir,       "Filename base to store output. e.g. --dest_dir=/data/ripd", :type => String, :required => true
+          opt :dest_pattern,   "Pattern for dump file output",                 :default => ":dest_dir/:handle_prefix/:handle/:date/:handle+:timestamp-:pid.tsv"
+        end
+        opts[:handle] ||= 'com.twitter'
+        scrape_config = YAML.load(File.open(ENV['HOME']+'/.monkeyshines'))
+        opts.merge! scrape_config
+
+      end
+
+      # ******************** Log ********************
+      def log
+        Monkeyshines.logger = Logger.new(opts[:log], 'daily') if opts[:log]
+        periodic_log = Monkeyshines::Monitor::PeriodicLogger.new(:iter_interval => 10000, :time_interval => 30)
+      end
+
+      # Source for requests
+      def src_store
+        @src_store = Monkeyshines::ScrapeStore::FlatFileStore.new_from_command_line(opts, :filemode => 'r')
+        @src_store.skip!(opts[:skip].to_i) if opts[:skip]
+      end
+
+      #
+      # ConditionalStore requests are only made (and thus data is only output)
+      # if the url is missing from the requested hash.
+      #
+      def dest_store
+        return @dest_store if @dest_store
+        # Track visited URLs with key-value database
+        @dest_cache = Monkeyshines::ScrapeStore::TyrantHdbKeyStore.new(opts[:cache_loc])
+        # Store the data into flat files
+        @dest_pattern = Monkeyshines::Utils::FilenamePattern.new(opts[:dest_pattern], :handle => opts[:handle], :dest_dir => opts[:dest_dir])
+        @dest_files   = Monkeyshines::ScrapeStore::ChunkedFlatFileStore.new(dest_pattern, opts[:chunk_time].to_i, opts)
+        # dest_store combines them
+        @dest_store = Monkeyshines::ScrapeStore::ConditionalStore.new(@dest_cache, @dest_files)
+      end
+
+      def engine
+        @scraper ||= Monkeyshines::ScrapeEngine::HttpScraper.new opts[:twitter_api]
+      end
+
+      #
+      # ******************** Do this thing ********************
+      #
+      def run
+        Monkeyshines.logger.info "Beginning scrape itself"
+        src_store.each do |req|
+          # If url key is missing,
+          result = dest_store.set(req.url) do
+            response = scraper.get(req)     # do the url fetch
+            next unless response.healthy?   # don't store bad fetches
+            [response.scraped_at, response] # timestamp into cache, result into flat file
+          end
+          periodic_log.periodically{ ["%7d"%dest_store.misses, 'misses', dest_store.size, req.response_code, result, req.url] }
+        end
+        src_store.close
+        dest_store.close
+        scraper.close
+      end
+
+    end
+  end
+end
