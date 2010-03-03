e88391277bc144b852f2cc7c0d8a8555b29550bc
Carl Knutson <carl@infochimps.org>
2010-03-03 16:04:03 -0600

    Removed trollop. Replaced with configliere

--- examples/shorturls/scrape_shorturls.rb
+++ examples/shorturls/scrape_shorturls.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
-$: << File.dirname(__FILE__)+'/../../lib'; $: << File.dirname(__FILE__)
+$: << File.dirname(__FILE__)+'/../../lib'; $: << File.dirname(__FILE__); $: << File.dirname(__FILE__)+'/../../../graphiterb/lib'
 require 'rubygems'
 require 'wukong'
 require 'monkeyshines'
+require 'configliere'
 #
 require 'shorturl_request'
 require 'shorturl_sequence'
@@ -10,7 +11,8 @@ require 'monkeyshines/utils/uri'
 require 'monkeyshines/utils/filename_pattern'
 require 'monkeyshines/store/conditional_store'
 require 'monkeyshines/fetcher/http_head_fetcher'
-require 'trollop' # gem install trollop
+require 'graphiterb' # needs graphiterb - simple ruby interface for graphite
+# require 'trollop' # gem install trollop
 
 # ===========================================================================
 #
@@ -26,40 +28,63 @@ require 'trollop' # gem install trollop
 #       --base-url="http://tinyurl.com" --min-limit= --max-limit= --encoding_radix=
 #
 #
-opts = Trollop::options do
-  opt :base_url,       "Host part of URL: eg tinyurl.com",             :type => String, :required => true
-  opt :log,            "Log file name; leave blank to use STDERR",     :type => String
-  # input from file
-  opt :from,           "Location of URLs to scrape",                   :type => String
-  opt :skip,           "Initial lines to skip",                        :type => Integer
-  # OR do a random walk
-  opt :random,         "Generate and visit random URL suffixes"
-  opt :min_limit,      "Smallest sequential URL to randomly visit",    :type => Integer # default in shorturl_sequence.rb
-  opt :max_limit,      "Largest sequential URL to randomly visit",     :type => Integer # default in shorturl_sequence.rb
-  opt :encoding_radix, "36 for most, 62 if URLs are case-sensitive",   :type => Integer, :default => 36
-  # output storage
-  opt :cache_loc,      "URI for cache server",                         :type => String
-  opt :chunk_time,     "Frequency to rotate chunk files (in seconds)", :type => Integer, :default => 60*60*4
-  opt :rootdir,       "Filename base for output, def /data/ripd",     :type => String,  :default => '/data/ripd'
-  opt :dest_pattern,   "Pattern for dump file output",                 :default => ":rootdir/:handle_prefix/:handle/:date/:handle+:timestamp-:pid.tsv"
-end
-handle = opts[:base_url].gsub(/\.com$/,'').gsub(/\W+/,'')
+
+Configliere.use :commandline, :config_file, :define
+Settings.read 'shorturls.yaml' #~/.configliere/shorturls.yaml
+Settings.define :base_url,     :description =>"Host part of URL: eg tinyurl.com", :type => String, :required => true
+Settings.define :log,          :description => "Log file name; leave blank to use STDERR", :type => String
+# input from file
+Settings.define :file_from,    :description => "Location of URLs to scrape", :type => String
+Settings.define :file_skip,    :description => "Initial lines to skip", :type => Integer
+# OR do a random walk
+Settings.define :random,       :description => "Generate and visit random URL suffixes"
+Settings.define :random_min,   :description => "Smallest sequential URL to randomly visit",    :type => Integer # default in shorturl_sequence.rb
+Settings.define :random_max,   :description => "Largest sequential URL to randomly visit",     :type => Integer # default in shorturl_sequence.rb
+Settings.define :random_radix, :description => "36 for most, 62 if URLs are case-sensitive",   :type => Integer, :default => 36
+# output storage
+Settings.define :cache_loc,      :description => "URI for cache server",                         :type => String
+Settings.define :chunk_time,     :description => "Frequency to rotate chunk files (in seconds)", :type => Integer, :default => 60*60*4
+Settings.define :rootdir,        :description => "Filename base for output, def /data/ripd",     :type => String,  :default => '/data/ripd'
+Settings.define :dest_pattern,   :description => "Pattern for dump file output",                 :default => ":rootdir/:handle_prefix/:handle/:date/:handle+:timestamp-:pid.tsv"
+Settings.resolve!
+
+# opts = Trollop::options do
+#   opt :base_url,       "Host part of URL: eg tinyurl.com",             :type => String, :required => true
+#   opt :log,            "Log file name; leave blank to use STDERR",     :type => String
+#   # input from file
+#   opt :from,           "Location of URLs to scrape",                   :type => String
+#   opt :skip,           "Initial lines to skip",                        :type => Integer
+#   # OR do a random walk
+#   opt :random,         "Generate and visit random URL suffixes"
+#   opt :min_limit,      "Smallest sequential URL to randomly visit",    :type => Integer # default in shorturl_sequence.rb
+#   opt :max_limit,      "Largest sequential URL to randomly visit",     :type => Integer # default in shorturl_sequence.rb
+#   opt :encoding_radix, "36 for most, 62 if URLs are case-sensitive",   :type => Integer, :default => 36
+#   # output storage
+#   opt :cache_loc,      "URI for cache server",                         :type => String
+#   opt :chunk_time,     "Frequency to rotate chunk files (in seconds)", :type => Integer, :default => 60*60*4
+#   opt :rootdir,       "Filename base for output, def /data/ripd",     :type => String,  :default => '/data/ripd'
+#   opt :dest_pattern,   "Pattern for dump file output",                 :default => ":rootdir/:handle_prefix/:handle/:date/:handle+:timestamp-:pid.tsv"
+# end
+handle = Settings.base_url.gsub(/\.com$/,'').gsub(/\W+/,'')
 
 # ******************** Log ********************
-opts[:log] = (WORK_DIR+"/log/shorturls_#{handle}-#{Time.now.to_flat}.log") if (opts[:log]=='')
+Settings.log = (WORK_DIR+"/log/shorturls_#{handle}-#{Time.now.to_flat}.log") if (Settings.log=='')
 periodic_log = Monkeyshines::Monitor::PeriodicLogger.new(:iters => 10000, :time => 30)
+
+# ******************** Graphite Sender ***********************
+graphite_sender = Graphiterb::GraphiteLogger.new(:iters => 1000, :time => 30)
 
 #
 # ******************** Load from store or random walk ********************
 #
-if opts[:from]
-  opts[:filename] = opts[:from]
-  src_store = Monkeyshines::Store::FlatFileStore.new opts # + {:filemode => 'r'}
-  src_store.skip!(opts[:skip].to_i) if opts[:skip]
-elsif opts[:random]
+if Settings.file_from
+  # Settings.filename = Settings.file_from
+  src_store = Monkeyshines::Store::FlatFileStore.new(:filename => Settings.file_from, :skip => Settings.file_skip.to_i) # + {:filemode => 'r'}
+  # src_store.skip!(Settings.file_skip.to_i) if Settings.file_skip
+elsif Settings.random
   src_store = Monkeyshines::Store::RandomUrlStream.new_from_command_line(opts)
 else
-  Trollop::die "Need to either say --random or --from=filename"
+  Settings.die "Need to either say --random or --file_from=filename"
 end
 
 #
@@ -68,7 +93,7 @@ end
 # Track visited URLs with key-value database
 #
 RDB_PORTS  = { 'tinyurl' => "localhost:10042", 'bitly' => "localhost:10043", 'other' => "localhost:10044" }
-cache_loc  = opts[:cache_loc] || RDB_PORTS[handle] or raise "Need a handle (bitly, tinyurl or other)."
+cache_loc  = Settings.cache_loc || RDB_PORTS[handle] or raise "Need a handle (bitly, tinyurl or other)."
 dest_cache = Monkeyshines::Store::TyrantRdbKeyStore.new(:uri => cache_loc)
 
 
@@ -77,10 +102,10 @@ dest_cache = Monkeyshines::Store::TyrantRdbKeyStore.new(:uri => cache_loc)
 #
 # Store the data into flat files
 #
-dest_pattern = Monkeyshines::Utils::FilenamePattern.new(opts[:dest_pattern],
-  :handle => 'shorturl-'+handle, :rootdir => opts[:rootdir])
-dest_files   = Monkeyshines::Store::ChunkedFlatFileStore.new(:pattern => opts[:dest_pattern],
-  :chunk_time => opts[:chunk_time].to_i, :rootdir => opts[:rootdir])
+dest_pattern = Monkeyshines::Utils::FilenamePattern.new(Settings.dest_pattern,
+  :handle => 'shorturl-'+handle, :rootdir => Settings.rootdir)
+dest_files   = Monkeyshines::Store::ChunkedFlatFileStore.new(:pattern => Settings.dest_pattern,
+  :chunk_time => Settings.chunk_time.to_i, :rootdir => Settings.rootdir)
 
 #
 # Conditional store uses the key-value DB to boss around the flat files --
